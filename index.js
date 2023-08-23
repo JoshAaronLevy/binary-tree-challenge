0ea3e340377e89070a5dbc8f40463a8d138ec2b5
JoshAaronLevy <joshaaronlevy@gmail.com>
2023-08-23 11:08:47 -0600

    Updated solution and tests

--- index.js
+++ index.js
@@ -1,47 +1,12 @@
 const solution = (arr) => {
   if (arr.length === 0) return "";
-  const [rootIndex, value] = findRootIndex(arr);
-  let leftSum = sum(leftNodeList(rootIndex, arr));
-  let rightSum = sum(rightNodeList(rootIndex, arr));
-  if (leftNodeList(rootIndex, arr).length === rightNodeList(rootIndex, arr).length) return "";
-  value === 100 ? rightSum = rightSum + 100 : rightSum = rightSum;
-  if (leftSum > rightSum) {
-    return "Left";
-  } else {
-    return "Right";
-  }
+  const { leftSum, rightSum } = arr.reduce((acc, value, index) => {
+    if (index === 0) return acc;
+    index % 2 === 1 ? acc.leftSum += value : acc.rightSum += value;
+    return acc;
+  }, { leftSum: 0, rightSum: 0 });
+  if (leftSum === rightSum) return "";
+  return leftSum > rightSum ? "Left" : "Right";
 };
 
-const sum = numbers => numbers.reduce((acc, curr) => acc + curr, 0);
-
-const findRootIndex = (arr) => {
-  let rootIndex = undefined;
-  let value = null;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === -1 || arr[i] === 100) {
-      rootIndex = i;
-      value = arr[i];
-    }
-  }
-  return [rootIndex, value];
-};
-
-const leftNodeList = (rootIndex, arr) => {
-  let leftSide = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i < rootIndex) {
-      leftSide.push(arr[i]);
-    }
-  }
-  return leftSide;
-};
-
-const rightNodeList = (rootIndex, arr) => {
-  let rightSide = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (i > rootIndex) {
-      rightSide.push(arr[i]);
-    }
-  }
-  return rightSide;
-};
+export default solution;
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,9 +1,20 @@
-var assert = require('assert');
+import { expect } from 'chai';
+import solution from '../index.js';
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+describe('Binary Tree Branch Sum', () => {
+  it('should return "Left" when the left branch is larger', () => {
+    expect(solution([3, 6, 2, 9, -1, 10])).to.equal('Left');
+  });
+
+  it('should return "Right" when the right branch is larger', () => {
+    expect(solution([3, 6, 2, 9, 100, 10])).to.equal('Right');
+  });
+
+  it('should return an empty string when the branches are equal', () => {
+    expect(solution([3, 6, 6, 9, 9, 10, 10])).to.equal('');
+  });
+
+  it('should return an empty string for an empty tree', () => {
+    expect(solution([])).to.equal('');
   });
 });
